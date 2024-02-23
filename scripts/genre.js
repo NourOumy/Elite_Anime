d9ed9d1498d07dc068f45f795730c608d02a0c09
NourOumy <ryahi.nourelhouda93@gmail.com>
2024-02-23 11:06:22 +0100

    ajout nour&alex + modif nom html

--- scripts/genre.js
+++ scripts/genre.js
@@ -73,6 +73,9 @@ var swiper = new Swiper(".mySwiper", {
     }
   }
   `;
+
+  // variables d'oliver :
+
   let titre1 = document.querySelector('.unun')
   let container1 = document.querySelector('.un')
   let container2 = document.querySelector('.deux')
@@ -80,6 +83,11 @@ var swiper = new Swiper(".mySwiper", {
   let container4 = document.querySelector('.quatre')
   let container5 = document.querySelector('.cinq')
   let container6 = document.querySelector('.six')
+
+  // variables de nour :
+let nav = document.querySelector('nav')
+let header = document.querySelector('header')
+
   // Définir les variables de la requête (dans ce cas, aucun besoin de spécifier un média spécifique)
   var variables = {};
   
@@ -106,6 +114,7 @@ var swiper = new Swiper(".mySwiper", {
   
       function searchAnimeBy(genre, container) {
         for (let i = 0; i < mediaList.Page.media.length; i++) {
+          header.style.background = `url(${mediaList.Page.media[i].bannerImage}) center/cover rgb(255,255,255)`
           for (let n = 0; n < mediaList.Page.media[i].genres.length; n++) {
             if (mediaList.Page.media[i].genres[n] == genre) {
               if (mediaList.Page.media[i].title.english == null) {
@@ -134,3 +143,30 @@ var swiper = new Swiper(".mySwiper", {
     })
     .catch(error => {console.log("Erreur lors de la récup des données :", error)});
   
+
+//event 3 boutons classe active
+
+nav.addEventListener("click", function(e){
+  // vérifier si l'élément sur lequel on click c'estt bien celui qui nous intéresse
+    if(e.target.classList.contains('button')){
+        // on vérifie si l'élément clické a la classe active, on le retire. si on met rien dans le if, c'estt d'office true
+      if(e.target.classList.contains('active')){
+        // si la réponse est true :
+        // toggle enlève si elle est là, met si elle n'est pas là
+        e.target.classList.toggle('active')
+        // si l'élément clické n'a pas la classe active, on l'ajoute'
+      } else {
+        
+      //  maintenant, on doit vérifier si un autre enfant a la classe Active, on doit le retirer
+        // Si le parent (wrapper) a un autre enfant (qque celui sur lequel je click) qui a la classe active ... 
+        if(nav.querySelector('.active')){
+          nav.querySelector('.active').classList.remove('active')
+            e.target.classList.add('active')
+          
+        // Si le parent (wrapper) n'a pas la classe active sur un des enfants on l'ajoutte sur le bttn clické
+        } else {
+            e.target.classList.toggle('active')
+        }
+    }
+    }
+})
